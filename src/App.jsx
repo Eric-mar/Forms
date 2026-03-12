addaed69a27b2ff2784b77211940e915d2ef4225
tuyishime eric <erictuyishime2004@gmail.com>
2026-03-12 11:15:40 +0200

    Adding the loading state and error state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState,useEffect } from 'react'
-
-import Forms from './components/Form'
-import Lists from './components/Lists'
+import React, { useState,useEffect } from 'react'
 import Custorm from './components/Custorm'
 
 function App() {
 
 return(
-  <>
-  {/* < Forms/>
-  <Lists/> */}
+  <div>
   < Custorm /> 
-  </>
+  </div>
 )
 
 }
--- src/components/Custorm.jsx
+++ src/components/Custorm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 // ### Exercise 4: useUsers Hook
 
 // - Create a **custom React hook** that fetches a list of users from:
@@ -12,8 +12,46 @@ import React from 'react'
 //     - Show an **error message** if the request fails.
 
 function Custorm() {
+    const [loading,setLoading]=useState(true)
+     const [error,setError]=useState(null)
+      const [data,setData]=useState([])
+      
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response)=> {
+            if(!response.ok){
+                throw new Error("failed to fetch!!!")
+            }
+            return response.json()
+        }
+        )
+        .then((data)=>
+          
+             setData(data) 
+            
+    )
+        .catch((error)=>setError(error.message))
+        .finally(()=> setLoading(false))
+    },[])
+
+    const filteredItems = data.map((el)=>{
+        return (
+            <div key={el.id} className='m-4 p-3 '>
+                <p>{el.name}</p>
+                <p>{el.email}</p>
+                <p>{el.address.city}</p>
+            </div>
+        )
+    })
+  
+  if (loading) return <h2>Loading data...</h2>
+  if (error) return <h2>Error while fetching: {error}</h2>
   return (
-    <div>Custorm</div>
+    <div>
+        
+            <div>{filteredItems}</div>
+    </div>
   )
 }
 
